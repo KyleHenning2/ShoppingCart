16f93d0f28bf3876508fe2845c98ac4298e5eaad
KyleHenning2 <kylehenning@u.boisestate.edu>
2024-02-20 22:06:01 -0700

    initial commit

---
+++ ShoppingCart/ShoppingCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCartApp.swift
+//  ShoppingCart
+//
+//  Created by Jennifer Henning on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
