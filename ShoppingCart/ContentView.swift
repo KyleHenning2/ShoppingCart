c0e7450ca265e9e0707b7ecb84011e8ad0648fc8
KyleHenning2 <kylehenning@u.boisestate.edu>
2024-02-20 22:34:24 -0700

    make it notes focused

--- ShoppingCart/ContentView.swift
+++ ShoppingCart/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var ListText: String = ""
+    @State private var titleText: String = ""
+
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Add your list name here", text: $titleText)
+                .font(.headline)
+                .padding()
+                .textFieldStyle(.roundedBorder)
+
+            Divider()
+            
+            TextEditor(text: $ListText)
+                .font(.headline)
+                .padding()
+                .background(Color.white)
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                .padding()
         }
         .padding()
+        .background(Color.gray.opacity(0.1))
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
